3e4f476fa29a7a2246d1249931e5febd9c210ec4
youlookwhat <770413277@qq.com>
2019-06-25 22:37:30 +0800

    add 添加网络请求、glide、状态栏、RecyclerView、rxbus基础库

---
+++ app/src/main/java/me/jingbin/mvpbinding/base/App.java
@@ -0,0 +1,43 @@
+package me.jingbin.mvpbinding.base;
+
+
+import android.content.ComponentCallbacks2;
+
+import androidx.multidex.MultiDexApplication;
+
+import com.bumptech.glide.Glide;
+
+
+public class App extends MultiDexApplication {
+
+    private static App app;
+
+    public static App getInstance() {
+        return app;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+
+//        HttpUtils.getInstance().init(this);
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        //当应用所有UI隐藏时应该释放UI上所有占用的资源
+        if (ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN == level) {
+            Glide.get(this).clearMemory();
+        }
+        //根据level级别来清除一些图片缓存
+        Glide.get(this).onTrimMemory(level);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        Glide.get(this).clearMemory();
+    }
+}
