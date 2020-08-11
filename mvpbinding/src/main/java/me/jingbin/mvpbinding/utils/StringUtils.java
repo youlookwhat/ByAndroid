a3e3b4554f877ef275b820fb25243142c8a0a17d
youlookwhat <770413277@qq.com>
2020-08-11 21:32:16 +0800

    update

---
+++ mvpbinding/src/main/java/me/jingbin/mvpbinding/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package me.jingbin.mvpbinding.utils;
+
+import android.content.Context;
+
+/**
+ * @author yeqing
+ * @description
+ * @time 2019/9/16
+ */
+public class StringUtils {
+    public static String getString(Context context, int sourceId) {
+        return context.getResources().getString(sourceId);
+    }
+}
